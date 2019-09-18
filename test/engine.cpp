d061b78a82390ff92a8829abc8013d673520d522
Mikhail Komarov <nemo@nil.foundation>
2019-09-18 05:43:47 +0300

    Minor test updates #1

--- test/engine.cpp
+++ test/engine.cpp
@@ -4,10 +4,6 @@
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/data/monomorphic.hpp>
 
-#include <nil/dcdb/engine.hpp>
-#include <nil/dcdb/sink.hpp>
-#include <nil/dcdb/layer.hpp>
-
 BOOST_AUTO_TEST_SUITE(storage_test_suite)
 
 BOOST_AUTO_TEST_SUITE_END()
